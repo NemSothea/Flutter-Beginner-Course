0786434d4111f94960dca9ff93cbc5669e92dfa0
nemsothea007 <nemsothea13@gmail.com>
2025-11-11 21:56:07 +0700

    navigationBottomBar

--- FirstApp/flutter_application_1/lib/pages/first_page.dart
+++ FirstApp/flutter_application_1/lib/pages/first_page.dart
@@ -1,36 +1,60 @@
 import 'package:flutter/material.dart';
-// import 'package:flutter_application_1/pages/second_page.dart';
+import 'package:flutter_application_1/pages/home_page.dart';
+import 'package:flutter_application_1/pages/profile_page.dart';
+import 'package:flutter_application_1/pages/setting_page.dart';
 
-class FirstPage extends StatelessWidget{
-  const FirstPage({super.key});
+class FirstPage extends StatefulWidget{
+  FirstPage({super.key});
+
+  @override
+  State<FirstPage> createState() => _FirstPageState();
+}
+
+class _FirstPageState extends State<FirstPage> {
+
+  int _selectedIndex = 0; 
+  
+  void _navigationBottomBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  final List _pages = [
+    //Home page
+    HomePage(),
+    // Profile
+    ProfilePage(),
+    //Settting 
+    SettingPage()
+  ];
 
   @override
   Widget build(BuildContext context) {
   return Scaffold(
-    appBar: AppBar(
-      title: Text("1st Page"),
-    ),
-    body: Center(
-      child: ElevatedButton(child: Text("Go to second Page",
-       style: TextStyle(
-        color: Colors.black,
-        fontSize: 20
-       ),
-      )
-      ,onPressed: () {
-        // Method of push original
-
-        // Navigator.push(context,MaterialPageRoute(
-        //   builder: (context) => SecondPage(),
-        // ));
-      
-      // Method of push using route
-      Navigator.pushNamed(context, '/secondpage');
-
-
-      },),
-    ),
+        appBar: AppBar(title: Text("1st Page")),
+        body: _pages[_selectedIndex],
+        bottomNavigationBar: 
+        BottomNavigationBar(
+          currentIndex: _selectedIndex,
+          onTap: _navigationBottomBar,
+          items: [
+          // Home
+            BottomNavigationBarItem(icon: 
+            Icon(Icons.home),
+            label: 'Home',
+            ),
+          // Profile
+          BottomNavigationBarItem(icon: 
+            Icon(Icons.person),
+            label: 'Profile',
+            ),
+          // Setting
+           BottomNavigationBarItem(icon: 
+            Icon(Icons.settings),
+            label: 'Setting',
+            ),
+        ]),
   );
   }
-
 } 
